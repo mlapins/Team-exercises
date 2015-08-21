1fb390c0cb6d79aed2066956501990744fbc3b6e
mlapins <matthewlapins@gmail.com>
2015-08-22 09:48:49 +1000

    Summarise data with tapply

--- ex1_answer.r
+++ ex1_answer.r
@@ -14,18 +14,56 @@ df <- data.frame(col1, col2, Col3)
 names(df) <- c("Buyer", "result","count")
 
 #Change the levels in the first column to "First Home Buyer", "Investor", "Refinancer"
-levels(df$Buyer) <- c("First Home Buyer", "Investor", "Refinancer")
+levels(df$Buyer) <- c("First.Home.Buyer", "Investor", "Refinancer")
 
 #Print the dataframe
 df
 
-
 #This is what you should end up with
 
-Buyer   result count
-1 First Home Buyer LMI-good    13
-2 First Home Buyer  LMI-bad    38
-3         Investor LMI-good    46
-4         Investor  LMI-bad    35
-5       Refinancer LMI-good    63
-6       Refinancer  LMI-bad     4
+#Buyer   result count
+#1 First Home Buyer LMI-good    13
+#2 First Home Buyer  LMI-bad    38
+#3         Investor LMI-good    46
+#4         Investor  LMI-bad    35
+#5       Refinancer LMI-good    63
+#6       Refinancer  LMI-bad     4
+
+
+
+
+
+#Summarise data using tapply
+
+result <- tapply(df$count, list(df$Buyer,df$result), mean)
+
+result <- t(result)
+
+barplot(result, beside = FALSE, legend = rownames(result))
+
+
+
+#Get count for plotting. Reshape grouping - Summarise using tapply and converting to a dataframe
+
+#df2 <- data.frame(tapply(df$count, list(df$Buyer,df$result), matrix))
+
+
+melt(df, id=c("Buyer","result"), measured="count")
+
+
+df2 <- t(df2)
+
+
+qplot(factor(df2$Investor))
+
+class(df2)
+
+library(ggplot2)
+
+
+
+#How to reshape data.
+library(reshape)
+
+df2 <- t(df)
+
